4d0cbe3fe338ade12ee94cbe90645c9397148488
Araminty <aramintywhitesell@gmail.com>
2020-02-23 00:42:10 -0500

    went back to just csv for KISS and put in a couple checks

--- app.py
+++ app.py
@@ -1,43 +1,67 @@
 from flask import Flask, jsonify, request
 import tabula
 import pandas as pd
+import pickle
+from os.path import isfile
 
 app = Flask(__name__)
 
 def csv_result(tables):
-    return [ table.to_csv() for table in tables ]
+    return jsonify(dict(enumerate([ table.to_csv() for table in tables ])))
 
-def json_result(tables):
-    return 'tables', tables 
+# def excel_result(tables):
+#     with pd.ExcelWriter('result.xlsx') as writer:
+#         for table in tables:
+#             table.to_excel(writer)
+#         return writer
 
-formaters = { 'csv': csv_result, 'json': json_result }
+# formaters = { 'csv': csv_result, 'excel': excel_result }
 
-def excel_result(tables):
-    with pd.ExcelWriter('result.xlsx') as writer:
-        for table in tables:
-            table.to_excel(writer)
-        return writer
-
-formaters = { 'csv': csv_result, 'json': json_result, 'excel': excel_result }
+files = {}
+if isfile('files_index.pkl'):
+    with open('files_index.pkl', 'rb') as file_index:
+        files = pickle.load(file_index)
 
 @app.route('/', methods=['POST', 'GET'])
 def handle():
-    urls = request.args.get('urls')
+    url = request.args.get('url')
     pages = request.args.get('pages')
-    formats = request.args.get('formats')
-    formats = [ format for format in formats if format in formaters ]
-    if not len(formats):
-        formats = ['csv']
+    # format = request.args.get('format')
+    print('url', url, 'pages', pages, 'formats', format)
 
-    tables=[]
-    if urls and pages:
-        for url in urls:
-            tables += tabula.read_pdf(url, pages=pages)
+    if url in files:
+        print('loading cached file...')
+        pdf = files[url]
+        print('done')
+    else:
+        print('downloading remote file...')
+        pdf = tabula.file_util.localize_file(url)[0]
+        print('done')
+        if pdf:
+            files[url]=pdf
+            with open('files_index.pkl', 'wb+') as file_index:
+                pickle.dump(files, file_index)
+
+    # if not format or format not in formaters:
+    #     format = 'csv'
+
+
+    int_check = lambda i: int(i) if type(i)==str and i.isdigit() else i if type(i)==int else None
+
+    if pages:
+        if pages=="all":
+            pass
+        else:
+            if type(pages)!=list:
+                pages = [ pages ]
+            pages = [ int_check(page) for page in pages ]
+    if not pages:
+        pages = "all"
+
+    tables= tabula.read_pdf(pdf, pages=pages)
+
     if not tables:
         return 'No matching tables found'
 
-    if request.method=='GET':
-        return csv_result(tables)
-    
-    return jsonify({ ( format, formaters[format](tables)) for format in formats })
+    return csv_result(tables)
 
